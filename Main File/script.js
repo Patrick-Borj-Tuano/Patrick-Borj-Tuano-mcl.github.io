f9092a76a10f287219df963ab6abf592df75649b
Patrick-Borj-Tuano <2022pbntuao@live.mcl.edu.ph>
2024-01-18 13:29:45 +0800

    Updated a lot of things

--- Main File/script.js
+++ Main File/script.js
@@ -4,3 +4,15 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+ document.querySelector('body').addEventListener('mousemove' , eyeball);
+        function eyeball() {
+    'use strict';
+    var eye = document.querySelectorAll(".eye");
+    eye.forEach(function (eye) {
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2)
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rot = (radian * (180 / Math.PI) * -1) + 270;
+        eye.style.transform  = "rotate("+ rot +"deg)"
+    })
+}
